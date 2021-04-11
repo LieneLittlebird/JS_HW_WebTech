5c29cb3eeee3a91508466ed0289c742adfa8bcc0
LieneLittlebird <liene.putnina20@gmail.com>
2021-04-11 16:46:30 +0300

    filled in the dropdowns

--- src/Form/CovidForm.jsx
+++ src/Form/CovidForm.jsx
@@ -4,33 +4,55 @@
 /* eslint-disable import/extensions */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 /* eslint-disable jsx-a11y/label-has-associated-control */
+
+// handle button cliks
+// setFilteredData(data.filter.magic)
 import { useState } from "react";
 
 import "./form.css";
 
 import data from "../data.js";
 
+const continentOptions = data
+  .map((x) => x.continent)
+  .filter((v, i, x) => x.indexOf(v) === i);
+
+const countryOptions = data
+  .map((x) => x.country)
+  .filter((v, i, x) => x.indexOf(v) === i);
+
 const CovidForm = () => {
-  const [continent, setContinent] = useState("");
-  // const chooseContinent = (event) => {
-  //   setContinent(event.continent);
-  // };
+  const showList = () => {
+    /* 
+    1. Grab all the fields (event.target.value?)
+    2. Check what's chosen
+    3. Post the results as tr 
+    3.1. Change cases weekly to deaths weekly and vice versa */
+  };
 
   return (
-    // handle button cliks
-    // setFilteredData(data.filter.magic)
-
     <div id="form">
       <div className="form-element">
         <label htmlFor="continent-select">Continent:</label>
         <select id="continent-select" defaultValue="selected">
           <option value="All">All</option>
-          {/* <option value="continent">{data.map(data,index)}</option> */}
+          {continentOptions.map((continentOption) => (
+            <option key={continentOption} value={continentOption}>
+              {continentOption}
+            </option>
+          ))}
         </select>
       </div>
       <div className="form-element">
         <label htmlFor="country-select">Country:</label>
-        <select id="country-select" />
+        <select id="country-select">
+          <option value="All">All</option>
+          {countryOptions.map((countryOption) => (
+            <option key={countryOption} value={countryOption}>
+              {countryOption}
+            </option>
+          ))}
+        </select>
       </div>
       <div className="form-element">
         <label htmlFor="year-from">Year from:</label>
@@ -61,6 +83,7 @@ const CovidForm = () => {
       <button id="show-list" type="submit" className="form-element">
         Show list
       </button>
+      {/* Button onClick={showList} */}
     </div>
   );
 };
